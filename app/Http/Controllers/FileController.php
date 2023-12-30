8679031a43fc6b044c6c4421580308de9226d6f9
Sumaiya <sumaiya.16.ete.cuet@gmail.com>
2023-12-30 09:14:24 +0600

    Multiple-image_upload

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Image;
+class FileController extends Controller
+{
+    public function fileUpload(Request $request){
+        $fileNames = [];
+            foreach($request->file('file') as $image){
+
+                $imageName =$image->getClientOriginalName();
+                $image->move(public_path().'/images/',$imageName);
+                $fileNames[] = $imageName;
+    }
+
+$images = json_encode($fileNames);
+Image::create(['images'=>$images]);
+return response()->json(['Data'=>$fileNames]);
+
+
+
+
+
+
+}
+
+}
+
+
+
+
---
+++ resources/views/files.blade.php
@@ -0,0 +1,10 @@
+<form action="" method="POST" enctype="multipart/form-data">
+@csrf
+<input type="file" name="file[]" multiple="multiple">
+<br>
+<br>
+<input type="submit">
+
+
+
+</form>
